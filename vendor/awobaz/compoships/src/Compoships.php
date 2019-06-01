3980366f617bbe1a699e235a73a5a245aaced84f
Wahyu Fajar <wahyufajar65@gmail.com>
2019-06-02 01:52:42 +0700

    test ignore

---
+++ vendor/awobaz/compoships/src/Compoships.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Awobaz\Compoships;
+
+use Awobaz\Compoships\Database\Eloquent\Concerns\HasRelationships;
+use Awobaz\Compoships\Database\Query\Builder as QueryBuilder;
+use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
+use Illuminate\Support\Str;
+
+trait Compoships
+{
+    use HasRelationships;
+
+    public function getAttribute($key)
+    {
+        if (is_array($key)) { //Check for multi-columns relationship
+            return array_map(function ($k) {
+                return parent::getAttribute($k);
+            }, $key);
+        }
+
+        return parent::getAttribute($key);
+    }
+
+    public function qualifyColumn($column)
+    {
+        if (is_array($column)) { //Check for multi-column relationship
+            return array_map(function ($c) {
+                if (Str::contains($c, '.')) {
+                    return $c;
+                }
+
+                return $this->getTable().'.'.$c;
+            }, $column);
+        }
+
+        return parent::qualifyColumn($column);
+    }
+
+    /**
+     * Configure Eloquent to use Compoships Eloquent Builder
+     *
+     * Create a new Eloquent query builder for the model.
+     *
+     * @param  \Illuminate\Database\Query\Builder  $query
+     * @return EloquentBuilder
+     */
+    public function newEloquentBuilder($query)
+    {
+        return new EloquentBuilder($query);
+    }
+
+    /**
+     * Configure Eloquent to use Compoships Query Builder
+     *
+     * @return \Awobaz\Compoships\Database\Query\Builder|static
+     */
+    protected function newBaseQueryBuilder()
+    {
+        $connection = $this->getConnection();
+
+        return new QueryBuilder($connection, $connection->getQueryGrammar(), $connection->getPostProcessor());
+    }
+}
---
+++ vendor/awobaz/compoships/src/Database/Eloquent/Concerns/HasRelationships.php
@@ -0,0 +1,174 @@
+<?php
+
+namespace Awobaz\Compoships\Database\Eloquent\Concerns;
+
+use Awobaz\Compoships\Database\Eloquent\Relations\BelongsTo;
+use Awobaz\Compoships\Database\Eloquent\Relations\HasMany;
+use Awobaz\Compoships\Database\Eloquent\Relations\HasOne;
+use Awobaz\Compoships\Exceptions\InvalidUsageException;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+trait HasRelationships
+{
+    /**
+     * Get the table qualified key name.
+     *
+     * @return mixed
+     */
+    public function getQualifiedKeyName()
+    {
+        $keyName = $this->getKeyName();
+
+        if (is_array($keyName)) { //Check for multi-columns relationship
+            $keys = [];
+
+            foreach ($keyName as $key) {
+                $keys[] = $this->getTable().$key;
+            }
+
+            return $keys;
+        }
+
+        return $this->getTable().'.'.$keyName;
+    }
+
+    /**
+     * Define a one-to-one relationship.
+     *
+     * @param  string  $related
+     * @param  string|array|null  $foreignKey
+     * @param  string|array|null  $localKey
+     * @return \Awobaz\Compoships\Database\Eloquent\Relations\HasOne
+     */
+    public function hasOne($related, $foreignKey = null, $localKey = null)
+    {
+        if (is_array($foreignKey)) { //Check for multi-columns relationship
+            $this->validateRelatedModel($related);
+        }
+
+        $instance = $this->newRelatedInstance($related);
+
+        $foreignKey = $foreignKey ?: $this->getForeignKey();
+
+        $foreignKeys = null;
+
+        if (is_array($foreignKey)) { //Check for multi-columns relationship
+            foreach ($foreignKey as $key) {
+                $foreignKeys[] = $this->sanitizeKey($instance, $key);
+            }
+        } else {
+            $foreignKey = $this->sanitizeKey($instance, $foreignKey);
+        }
+
+        $localKey = $localKey ?: $this->getKeyName();
+
+        return new HasOne($instance->newQuery(), $this, $foreignKeys ?: $foreignKey, $localKey);
+    }
+
+    /**
+     * Validate the related model for Compoships compatibility
+     *
+     * @param  $related
+     * @return void
+     * @throws InvalidUsageException
+     */
+    private function validateRelatedModel($related)
+    {
+        $uses = class_uses_recursive($related);
+
+        if (! array_key_exists('Awobaz\Compoships\Compoships', $uses) && ! is_subclass_of($related,
+                'Awobaz\Compoships\Database\Eloquent\Model')) {
+            throw new InvalidUsageException("The related model '${related}' must either extend 'Awobaz\Compoships\Database\Eloquent\Model' or use the 'Awobaz\Compoships\Compoships' trait");
+        }
+    }
+
+    /**
+     * Define a one-to-many relationship.
+     *
+     * @param  string  $related
+     * @param  string|array|null  $foreignKey
+     * @param  string|array|null  $localKey
+     * @return \Awobaz\Compoships\Database\Eloquent\Relations\HasMany
+     */
+    public function hasMany($related, $foreignKey = null, $localKey = null)
+    {
+        if (is_array($foreignKey)) { //Check for multi-columns relationship
+            $this->validateRelatedModel($related);
+        }
+
+        $instance = $this->newRelatedInstance($related);
+
+        $foreignKey = $foreignKey ?: $this->getForeignKey();
+
+        $foreignKeys = null;
+
+        if (is_array($foreignKey)) { //Check for multi-columns relationship
+            foreach ($foreignKey as $key) {
+                $foreignKeys[] = $this->sanitizeKey($instance, $key);
+            }
+        } else {
+            $foreignKey = $this->sanitizeKey($instance, $foreignKey);
+        }
+
+        $localKey = $localKey ?: $this->getKeyName();
+
+        return new HasMany($instance->newQuery(), $this, $foreignKeys ?: $foreignKey, $localKey);
+    }
+
+    /**
+     * Define an inverse one-to-one or many relationship.
+     *
+     * @param  string  $related
+     * @param  string|array|null  $foreignKey
+     * @param  string|array|null  $ownerKey
+     * @param  string  $relation
+     * @return \Awobaz\Compoships\Database\Eloquent\Relations\BelongsTo
+     */
+    public function belongsTo($related, $foreignKey = null, $ownerKey = null, $relation = null)
+    {
+        if (is_array($foreignKey)) { //Check for multi-columns relationship
+            $this->validateRelatedModel($related);
+        }
+
+        // If no relation name was given, we will use this debug backtrace to extract
+        // the calling method's name and use that as the relationship name as most
+        // of the time this will be what we desire to use for the relationships.
+        if (is_null($relation)) {
+            $relation = $this->guessBelongsToRelation();
+        }
+
+        $instance = $this->newRelatedInstance($related);
+
+        // If no foreign key was supplied, we can use a backtrace to guess the proper
+        // foreign key name by using the name of the relationship function, which
+        // when combined with an "_id" should conventionally match the columns.
+        if (is_null($foreignKey)) {
+            $foreignKey = Str::snake($relation).'_'.$instance->getKeyName();
+        }
+
+        // Once we have the foreign key names, we'll just create a new Eloquent query
+        // for the related models and returns the relationship instance which will
+        // actually be responsible for retrieving and hydrating every relations.
+        $ownerKey = $ownerKey ?: $instance->getKeyName();
+
+        return new BelongsTo($instance->newQuery(), $this, $foreignKey, $ownerKey, $relation);
+    }
+
+    /**
+     * Honor DB::raw instances
+     *
+     * @param  string  $instance
+     * @param  string  $foreignKey
+     * @return string|Expression
+     */
+    protected function sanitizeKey($instance, $foreignKey)
+    {
+        $grammar = $this->getConnection()
+            ->getQueryGrammar();
+
+        return $grammar->isExpression($foreignKey)
+            ? DB::raw($instance->getTable().'.'.$foreignKey)
+            : $instance->getTable().'.'.$foreignKey;
+    }
+}
